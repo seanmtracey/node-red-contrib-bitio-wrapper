4c80799f09e793467b545b1394cad6ca8ff84358
Sean Tracey <seanmtracey@gmail.com>
2018-01-12 11:33:23 +0000

    Basic structure of interface

---
+++ execute.py
@@ -0,0 +1,7 @@
+import sys
+# import microbit
+
+FUNCTION = sys.argv[1]
+VALUE = sys.argv[2]
+
+print(FUNCTION, VALUE)
--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-const spawn = require('child_process').spawn;
+const debug = require('debug');
+const { spawn } = require('child_process');
 
-module.exports = function(){
+function executeMicrobitCommand(command, options){
 
-};
+	return new Promise( (resolve, reject) => {
+
+		const arguments = [
+			'execute.py',
+			`scrolling-text`,
+			"Hello!"
+		];
+		
+		const microbitProcess = spawn('python', arguments);
+
+		microbitProcess.stdout.on('data', (data) => {
+			console.log(`node-red-contrib-bitio-wrapper: stdout: ${data}`);
+		});
+		
+		microbitProcess.stderr.on('data', (data) => {
+			console.log(`node-red-contrib-bitio-wrapper: stderr: ${data}`);
+		});
+		
+		microbitProcess.on('close', (code) => {
+			console.log(`node-red-contrib-bitio-wrapper: child process exited with code ${code}`);
+
+			if(code === 0){
+				resolve();
+			} else {
+				reject();
+			}
+
+		});
+
+	});
+
+}
+
+module.exports = function(RED) {
+		
+	function bitio(config) {
+		
+		RED.nodes.createNode(this, config);
+		
+		var node = this;
+		
+		node.on('input', function(msg) {
+			/*msg.payload = msg.payload.toLowerCase();
+			node.send(msg);*/
+		});
+		
+	}
+		
+	RED.nodes.registerType("bitio", bitio);
+
+}
+
+executeMicrobitCommand();
